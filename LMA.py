5c5e31f81a067c372721dc98b423dfdf8933ff27
Francisco Simões <francisco8857@gmail.com>
2020-04-29 03:11:22 +0100

    Update LMA.py

--- LMA.py
+++ LMA.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from SABR import impVol as vol
 
 
@@ -39,9 +41,53 @@ def d2VoldRhodVv(alpha, beta, rho, Vv, K, f, T, h):
 
     return d
 
+def dVoldAlpha(alpha, beta, rho, Vv, K, f, T, h):
+
+    d = vol(alpha+h, beta, rho, Vv, K, f, T) - vol(alpha-h, beta, rho, Vv, K, f, T)/2*h
+
+    return d
+
+def dVoldRho(alpha, beta, rho, Vv, K, f, T, h):
+
+    d = vol(alpha, beta, rho + h, Vv, K, f, T) - vol(alpha, beta, rho - h, Vv, K, f, T)/2*h
+
+    return d
+
+def dVoldVv(alpha, beta, rho, Vv, K, f, T, h):
+
+    d = vol(alpha, beta, rho, Vv + h, K, f, T) - vol(alpha, beta, rho, Vv - h, K, f, T)/2*h
+
+    return d
+
+def NumericJacobian(quote, alpha, rho, Vv, beta):
+
+
+    f, duration, strike= quote[0], quote[2], quote[3]
+
+    dAlpha = []
+    dRho = []
+    dVv = []
+
+    h = 0.0001
+
+    for i in np.arange(len(strike)):
+
+        dAlpha.append(dVoldAlpha(alpha, beta, rho, Vv, strike[i], f[i], duration[i], h))
+        dRho.append(dVoldRho(alpha, beta, rho, Vv, strike[i], f[i], duration[i], h))
+        dVv.append(dVoldVv(alpha, beta, rho, Vv, strike[i], f[i], duration[i], h))
+
+    J = np.transpose(np.asmatrix(np.asarray([dAlpha, dRho,dVv])))
+
+    return J
 
 
 def LMA (quotes, beta):
+
+    alpha = 0.05
+
+    rho = -0.4
+
+    Vv = 0.3
 
     count = 0  # cycle counter
 
@@ -50,6 +96,10 @@ def LMA (quotes, beta):
     epsilon = 1  # initial value for the error epsilon
 
     tol = 1e-3  # break condition for the size of epsilon
+
+    J = NumericJacobian(quotes, alpha, rho, Vv, beta)
+
+
 
     while epsilon > tol:  # Newton-Raphson's method
 
