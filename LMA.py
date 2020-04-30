4c04380a97db7f9bf030d17598f1c2ab308d296f
Francisco Simões <francisco8857@gmail.com>
2020-04-30 11:52:40 +0100

    Update LMA.py

--- LMA.py
+++ LMA.py
@@ -80,6 +80,12 @@ def NumericJacobian(quote, alpha, rho, Vv, beta):
 
     return JT
 
+def WeightedHessian(JT, W):
+
+    H = np.matmul(JT, np.matmul(W, np.transpose(JT)))
+
+    return H
+
 def diferenceVector(quote, v, alpha, rho, Vv, beta):
 
     f, duration, strike = quote[0], quote[2], quote[3]
@@ -98,11 +104,9 @@ def diferenceVector(quote, v, alpha, rho, Vv, beta):
 
     return Y
 
-def getStep(quote, vol, alpha, rho, Vv, beta, Lambda, W):
+def getStep(quote, vol, alpha, rho, Vv, beta, Lambda, W, JT):
 
-    JT = NumericJacobian(quote, alpha, rho, Vv, beta)  # jacobian transposed
-
-    H = np.matmul(JT, np.matmul(W, np.transpose(JT)))
+    H = WeightedHessian(JT, W)
 
     diagH = np.diag(np.diagonal(H))
 
@@ -135,6 +139,26 @@ def WeightMatrix(vol):
 
     return W
 
+def Improved(quote, vol, alpha, rho, Vv, beta, Lambda, W, JT, h):
+
+    eps = 0.5
+
+    hAlpha, hRho, hVv = float(h[0]), float(h[1]), float(h[2])
+
+    H = WeightedHessian(JT, W)
+
+    Y = diferenceVector(quote, vol, alpha, rho, Vv, beta)
+
+    diagH = np.diag(np.diagonal(H))
+
+    d = Chi2(quote, vol, alpha, rho, Vv, beta, W) - Chi2(quote, vol, alpha+hAlpha, rho+hRho, Vv+hVv, beta, W)
+
+    n = float(np.matmul(np.transpose(h), (np.matmul(Lambda * diagH, h) + np.matmul(np.matmul(JT, W), Y))))
+
+    r = d/n
+
+    return r > eps
+
 def LMA (quote, vol, beta):
 
     alpha = 0.05
@@ -145,10 +169,14 @@ def LMA (quote, vol, beta):
 
     Lambda = 1000
 
+    JT = NumericJacobian(quote, alpha, rho, Vv, beta)
+
     W = WeightMatrix(vol)
 
-    h = getStep(quote, vol, alpha, rho, Vv, beta, Lambda, W)
+    h = getStep(quote, vol, alpha, rho, Vv, beta, Lambda, W, JT)
 
     X = Chi2(quote, vol, alpha, rho, Vv, beta, W)
 
-    print(X, h)
+    i = Improved(quote, vol, alpha, rho, Vv, beta, Lambda, W, JT, h)
+
+    print(i)
